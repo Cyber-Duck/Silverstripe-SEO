3a01968dda0a92b3bf6a17e8cb5d78887ed7a29a
Andrew McCormack <andrewm@cyber-duck.co.uk>
2016-01-08 16:40:23 +0000

    add sitemap xml basic logic

--- code/controllers/SitemapXML.php
+++ code/controllers/SitemapXML.php
@@ -12,11 +12,23 @@ class SitemapXML_Controller extends Page_Controller {
 
     public function init()
     {
+        parent::init();
 
+        $this->getSiteTree();
     }
 
     public function GetSitemapXML()
     {
+        $sitemap = new ArrayData(array(
+            'Pages' => $this->pages
+        ));
+        return $sitemap->renderWith('SitemapPages');
+    }
 
+    private function getSiteTree()
+    {
+        $this->pages = SiteTree::get()->filter(array(
+            'ClassName:not' => 'ErrorPage'
+        ));
     }
 }
