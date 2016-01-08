d5db4e3a7ce4480662eb75b7da8243f3aa7d504a
Andrew McCormack <andrewm@cyber-duck.co.uk>
2016-01-08 15:53:22 +0000

    add xml sitemap route and controller

---
+++ code/controllers/SitemapXML.php
@@ -0,0 +1,22 @@
+<?php
+
+class SitemapXML_Controller extends Page_Controller {
+
+    private static $url_handlers = array(
+        '' => 'GetSitemapXML'
+    );
+
+    private static $allowed_actions = array(
+        'GetSitemapXML'
+    );
+
+    public function init()
+    {
+
+    }
+
+    public function GetSitemapXML()
+    {
+
+    }
+}
