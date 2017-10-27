5323e4084284d7b3d098face4c33ab5f4a78386e
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2017-10-27 16:13:02 +0100

    get parent CMS fields in seo extension

--- src/Model/Extension/SeoExtension.php
+++ src/Model/Extension/SeoExtension.php
@@ -32,6 +32,8 @@ class SeoExtension extends SeoPageExtension
 
     public function updateCMSFields(FieldList $fields) 
     {
+        $fields = parent::updateCMSFields($fields);
+
         $fields->addFieldToTab('Root.Main', TextField::create('Title'));
 
         return $fields;
