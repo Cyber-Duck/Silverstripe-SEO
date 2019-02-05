745b0ab027014a33bb11f9a6efb00942c545e47b
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2019-02-05 15:48:07 +0000

    fix seo paginated list prev link when request is not an object

--- src/ORM/SeoPaginatedList.php
+++ src/ORM/SeoPaginatedList.php
@@ -2,22 +2,25 @@
 
 namespace CyberDuck\SEO\ORM;
 
+use SilverStripe\Control\Controller;
 use SilverStripe\Control\HTTP;
+use SilverStripe\Control\HTTPRequest;
 use SilverStripe\ORM\PaginatedList;
 
-class SeoPaginatedList extends PaginatedList 
+class SeoPaginatedList extends PaginatedList
 {
     public function PrevLink()
     {
-        if($this->CurrentPage() == 2) {
-            $url = $this->request->getURL(false);
+        if ($this->CurrentPage() == 2) {
+            $request = $this->request instanceof HTTPRequest ? $this->request : Controller::curr()->getRequest();
+            $url = $request->getURL(false);
 
             $sortingVar = $this->getPaginationGetVar();
-            $getVars = $this->request->getVars();
-            if(array_key_exists($sortingVar, $getVars)) {
+            $getVars = $request->getVars();
+            if (array_key_exists($sortingVar, $getVars)) {
                 unset($getVars[$sortingVar]);
             }
-            foreach($getVars as $key => $value) {
+            foreach ($getVars as $key => $value) {
                 $url = HTTP::setGetVar($key, $value, $url);
             }
             return $url;
